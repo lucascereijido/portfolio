a77493c5adb57a8c9076c725db87c40ceadd6905
fjswayze <fjswayze@vassar.edu>
2021-01-19 10:36:31 -0800

    Updates

--- src/components/loading/loading.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import loading from '../../assets/gif/loading.gif'
-export default function Loading() {
-    return (<div className="loading">
-       <img src={loading}></img>
-    </div>)
-}
