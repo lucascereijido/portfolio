ac22a09ff757bb26a12cdf7b83d03d6a927ce5ee
fjswayze <fjswayze@vassar.edu>
2021-01-21 09:51:18 -0800

    youtube and squarz

--- src/components/work/show/2d_animation.jsx
+++ src/components/work/show/2d_animation.jsx
@@ -9,8 +9,8 @@ export default function Animation(props){
    return( 
    <div className="content">
            <div className='quarantony'>
-               <iframe title="QUARAN-TONY" width="420" height="315" src="https://www.youtube.com/watch?v=IVxoitrsATg&ab_channel=lucascereijido">
-               </iframe>
+               <iframe width="560" height="315" src="https://www.youtube.com/embed/IVxoitrsATg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="hello"></iframe>
+
                <div>QUARAN-TONY details a dull and relatable day in the life of Tony, spirit animal of ~quarantine~</div>
            </div>
        
